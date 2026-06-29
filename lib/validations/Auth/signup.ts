6f67fda8a35f2378829ec52a1aa5960e52a63add
Raj Mishra <raj212558@gmail.com>
2026-06-29 22:06:45 +0530

    chore: update auth validations

--- lib/validations/Auth/signup.ts
+++ lib/validations/Auth/signup.ts
@@ -3,6 +3,8 @@ import { z } from "zod";
 
 // Sign Up
 export const SignUpSchema = z.object({
+  // firstName: z.string().min(2, "Name must be at least 2 characters.").max(30),
+  // lastName: z.string().min(2, "Name must be at least 2 characters.").max(30),
   email: z.string().email("Please enter a valid email address."),
   password: z.string().min(6, "Password must be at least 6 characters."),
 });
