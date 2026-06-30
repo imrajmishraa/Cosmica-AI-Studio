9b1c93c70a80886aa7e15d2917638acaa465d21b
Raj Mishra <raj212558@gmail.com>
2026-06-30 14:53:11 +0530

    feat: add signUp component by clerk

--- lib/validations/Auth/signin.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-// Sign In
-export const SignInSchema = z.object({
-  email: z.string().email("Please enter a valid email address."),
-  password: z.string().min(6, "Password must be at least 6 characters."),
-});
-
-export type SignInIndex = z.infer<typeof SignInSchema>;
